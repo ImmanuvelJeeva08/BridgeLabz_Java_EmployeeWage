cfe728d04eef95a4338063a34cec6ba224196fed
immanuveljeeva08 <immanuveljeeva2000@gmail.com>
2021-07-11 09:52:01 -0700

    Use Case2

---
+++ Uc2.java
@@ -0,0 +1,15 @@
+/*****************************************************************************************************************
+  *********************************** Calculate Employee Wage Per Day ********************************************
+  ****************************************************************************************************************/
+public class Uc2 {
+
+    // Declaration Part
+    private static final int WagePerHour=20;
+    private static final int FullDayHour=8;
+
+    public static void main(String[] args) {
+
+        int sum=WagePerHour*FullDayHour;
+        System.out.println("Employee Wage Per Day = "+sum);
+    }
+}
